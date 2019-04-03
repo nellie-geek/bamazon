d1d8e62085cca8e928536d750b4e8e465b8a26d4
nellie-geek <janellegieck@msn.com>
2019-04-02 20:24:48 -0600

    Added schema and seeds files.

---
+++ seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("Dog Food", "Pet Supplies", 68.20, 14),
+("Diamond Earrings", "Jewelry", 1199.99, 7),
+("Ready Player One by Ernest Cline", "Books", 19.74, 185),
+("Phone Case", "Cell Phones and Accessories", 24.62, 2),
+("Face Lotion", "Beauty and Personal Care", 18.95, 24),
+("Bananas", "Grocery", 1.79, 128),
+("KitchenAid", "Home and Kitchen", 279.00, 87),
+("Magic 8 Ball", "Toys and Games", 11.43, 3),
+("Power Drill", "Tools", 60.58, 102),
+("Sticky Notes", "Office Products", 7.26, 19)
+;
