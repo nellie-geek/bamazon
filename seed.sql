73f850ecf0dc1d0bde13be814a37925fddcb062b
nellie-geek <janellegieck@msn.com>
2019-04-02 20:32:25 -0600

    Updated sql files.

--- seed.sql
+++ seed.sql
@@ -1,11 +1,11 @@
 INSERT INTO products (product_name, department_name, price, stock_quantity) 
 VALUES ("Dog Food", "Pet Supplies", 68.20, 14),
 ("Diamond Earrings", "Jewelry", 1199.99, 7),
-("Ready Player One by Ernest Cline", "Books", 19.74, 185),
-("Phone Case", "Cell Phones and Accessories", 24.62, 2),
-("Face Lotion", "Beauty and Personal Care", 18.95, 24),
+("Dove Shampoo", "Personal Care", 5.75, 16),
+("Phone Case", "Cell Phone Accessories", 24.62, 2),
+("Face Lotion", "Personal Care", 18.95, 24),
 ("Bananas", "Grocery", 1.79, 128),
-("KitchenAid", "Home and Kitchen", 279.00, 87),
+("KitchenAid Blender", "Kitchen", 279.00, 87),
 ("Magic 8 Ball", "Toys and Games", 11.43, 3),
 ("Power Drill", "Tools", 60.58, 102),
 ("Sticky Notes", "Office Products", 7.26, 19)
